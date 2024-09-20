fa6b49b11eee90deae976a63ccce5341a767b435
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-09-20 16:15:35 +0300

    some fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export const App = () => {
     return (
-        <main className='relative flex items-center justify-center w-full max-w-[1440px] min-h-screen p-[24px] m-auto'>
+        <main className='relative w-full max-w-[1440px] m-auto'>
             <Outlet />
         </main>
     );
